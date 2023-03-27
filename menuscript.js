4ac24d4007143222a444b1a80930bc6fb549c0e2
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2023-03-27 11:24:54 +0300

    Add files via upload

---
+++ menuscript.js
@@ -0,0 +1,11 @@
+const loader = document.getElementById("loader");
+
+
+
+window.onload = function() {
+    // Get a reference to the loader element
+    var loader = document.getElementById("loader");
+  
+    // Hide the loader
+    loader.style.display = "none";
+}
